a607bb7adf61284fae6f5a7f4c105eef07fe0f09
psreeteja <psteja02@gmail.com>
2025-05-25 13:07:51 +0530

    Initial commit

---
+++ test/canary-dep.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CanaryDep from '../lib/canary-dep-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/canary-dep-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CanaryDep.CanaryDepStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
